3220194501448ccf2413d1bc29452f90d479f9a8
erastov-alex <erastov.alex@gmail.com>
2023-09-27 17:32:40 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Electric Wires"
 PROJECT_NAME_FULL: str = "Auto-generated Wires for Semantic Segmentation with Domain-Independence (Synthetic Dataset)"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
